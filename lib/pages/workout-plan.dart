2546bdc88176fa95d7ec048d679cc0c80bf18770
Tony <tonynguyen0925@gmail.com>
2020-04-17 18:11:15 -0700

    merged list with some UI

--- lib/pages/workout-plan.dart
+++ lib/pages/workout-plan.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:fitness_app/services/shared-pref-helper.dart';
 
 class WorkoutPlan extends StatefulWidget {
   @override
@@ -6,6 +9,47 @@ class WorkoutPlan extends StatefulWidget {
 }
 
 class _WorkoutPlanState extends State<WorkoutPlan> {
+  SharedPreferences prefs;
+
+  String _motivation;
+  int _plan;
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+    setData();
+  }
+
+  void _init() async {
+    prefs = await SharedPreferences.getInstance();
+  }
+
+  void setData() {
+    SharedPreferencesHelper.getMotivation(prefs).then((motivation) {
+      setState(() {
+        this._motivation = motivation;
+        if (motivation == 'I want to gain strength') {
+          _plan = 0;
+        } else if (motivation == 'I want to lose weight') {
+          _plan = 1;
+        } else {
+          _plan = 2;
+        }
+      });
+    });
+  }
+
+  Widget customPlan() {
+    if (_plan == 0) {
+      return Text('Put strength workouts here');
+    } else if (_plan == 0) {
+      return Text('Put slimming workouts here');
+    } else {
+      return Text('ERROR');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +62,29 @@ class _WorkoutPlanState extends State<WorkoutPlan> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Center(
-              child: Text('Fill customized workout plan here'),
+            Expanded(
+              child: Center(
+                child: customPlan(),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: ButtonTheme(
+                    //male button
+                    minWidth: double.infinity,
+                    height: 50.0,
+                    child: RaisedButton(
+                      onPressed: () {},
+                      child: Text('DONE'),
+                      shape: RoundedRectangleBorder(),
+                      color: Colors.blue, //TODO: Pick a Color
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
