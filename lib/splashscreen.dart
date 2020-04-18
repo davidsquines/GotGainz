094397dc7b0e8db47b2f780571270a95fc2b8eae
ayeejane <jane.cabanayan.463@my.csun.edu>
2020-04-18 16:57:45 -0700

    added app launcher icon

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,3 +1,4 @@
+import 'package:fitness_app/pages/onboarding-builder.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_splash/animated_splash.dart';
 import 'package:fitness_app/pages/root-page.dart';
@@ -8,7 +9,7 @@ class SplashScreen extends StatefulWidget {
 }
 
 Map<int, Widget> op = {
-  1: RootPage()
+  1: Onboarding()
 }; //TODO Change this back to RootPage after Testing
 
 Function duringSplash = () {
@@ -22,7 +23,7 @@ class _SplashScreenState extends State<SplashScreen> {
       debugShowCheckedModeBanner: false,
       home: AnimatedSplash(
         imagePath: 'assets/gifs/splashScreen.gif',
-        home: RootPage(), //TODO Change this back to RootPage after Testing
+        home: Onboarding(), //TODO Change this back to RootPage after Testing
         customFunction: duringSplash,
         duration: 2300, //TODO: May need to update duration
         type: AnimatedSplashType.BackgroundProcess,
