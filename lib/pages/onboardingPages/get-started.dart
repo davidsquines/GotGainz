deb563b645e9530f65e3f20bad7da34f8b99d256
Tony <tonynguyen0925@gmail.com>
2020-04-18 19:41:55 -0700

    cleaned up get-started page

--- lib/pages/onboardingPages/get-started.dart
+++ lib/pages/onboardingPages/get-started.dart
@@ -3,48 +3,60 @@ import 'package:flutter/material.dart';
 class GetStarted extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.lightBlueAccent, //TODO: Pick a color
-          elevation: 0.0,
-        ),
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
-                  child: Text(
-                    'A FEW QUESTIONS BEFORE WE GET STARTED',
-                    style: TextStyle(
-                      fontSize: 22,
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 200,
-                ),
-                ButtonTheme(
-                  minWidth: 200.0,
-                  height: 50.0,
-                  child: FlatButton(
-                    onPressed: () {
-                      Navigator.of(context).pushNamed(
-                        '/second',
-                      );
-                    },
-                    child: Text('GET STARTED'),
-                    shape: RoundedRectangleBorder(),
-                    color: Colors.lightBlueAccent, //TODO: Pick a Color
-                  ),
-                ),
-              ],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: EdgeInsets.fromLTRB(25, 100, 25, 25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Your Personal',
+              style: TextStyle(
+                color: Colors.lightBlue,
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
+            Text(
+              'Fitness Trainer'.toUpperCase(),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 48.0,
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              'A few thing before you start working out.',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 16.0,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(
+                  '/second',
+                );
+              },
+              minWidth: double.infinity,
+              height: 50.0,
+              child: Text(
+                'Get Started'.toUpperCase(),
+              ),
+              color: Colors.lightBlue,
+              textColor: Colors.white,
+            ),
+          ],
         ),
       ),
     );
