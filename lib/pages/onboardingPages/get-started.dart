faab0921896590d248507a2d739161070d917ab9
Tony <tonynguyen0925@gmail.com>
2020-04-20 12:05:39 -0700

    testing web support

--- lib/pages/onboardingPages/get-started.dart
+++ lib/pages/onboardingPages/get-started.dart
@@ -6,65 +6,67 @@ class GetStarted extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        padding: EdgeInsets.fromLTRB(25, 100, 25, 25),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image(
-              image:AssetImage('assets/getStartedImage.png'),
-              width: 380.0,
-              height: 360.0,
-              fit: BoxFit.fill,
-            ),
-            Text(
-              'Welcome to Got Gainz',
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.fromLTRB(25, 100, 25, 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image(
+                image: AssetImage('assets/getStartedImage.png'),
+                width: 380.0,
+                height: 360.0,
+                fit: BoxFit.fill,
+              ),
+              Text(
+                'Welcome to Got Gainz',
                 textAlign: TextAlign.left,
                 style: TextStyle(
-                color: Colors.lightBlue,
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
+                  color: Colors.lightBlue,
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            Text(
-              'Your personal fitness trainer is waiting for you.',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 16.0,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
+              Text(
+                'Your personal fitness trainer is waiting for you.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              'A few things before you start working out.',
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 16.0,
+              SizedBox(
+                height: 30.0,
               ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            MaterialButton(
-              minWidth: 30.0,
-              height: 50.0,
-              onPressed: () {
-                Navigator.of(context).pushNamed(
-                  '/second',
-                );
-              },
-              child: Text(
-                'Get Started'.toUpperCase(),
+              Text(
+                'A few things before you start working out.',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 16.0,
+                ),
+                textAlign: TextAlign.center,
               ),
-              color: Colors.lightBlueAccent,
-              textColor: Colors.white,
-            ),
-          ],
+              SizedBox(
+                height: 30.0,
+              ),
+              MaterialButton(
+                minWidth: 30.0,
+                height: 50.0,
+                onPressed: () {
+                  Navigator.of(context).pushNamed(
+                    '/second',
+                  );
+                },
+                child: Text(
+                  'Get Started'.toUpperCase(),
+                ),
+                color: Colors.lightBlueAccent,
+                textColor: Colors.white,
+              ),
+            ],
+          ),
         ),
       ),
     );
