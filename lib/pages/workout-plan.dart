fa67b98a704ab2100be3efa7d94bf59052d196dd
Tony <tonynguyen0925@gmail.com>
2020-04-19 17:02:24 -0700

    added an alert if user has not completed the plan

--- lib/pages/workout-plan.dart
+++ lib/pages/workout-plan.dart
@@ -17,7 +17,11 @@ class _WorkoutPlanState extends State<WorkoutPlan> {
   SharedPreferences prefs;
 
   String motivation;
-  String apiLink = '';
+  String _apiLink = '';
+  int _planLength;
+
+  int currentLevelProgress = 1;
+  int userLevel = 1;
 
   @override
   void initState() {
@@ -37,14 +41,30 @@ class _WorkoutPlanState extends State<WorkoutPlan> {
           () {
             this.motivation = motivation;
             if (motivation == 'I want to gain strength') {
-              apiLink =
-                  'http://www.json-generator.com/api/json/get/cfzyZlABqq?indent=2'; //TODO: Change this link this a different one that contains a premade strength workout
+              if (userLevel == 1) {
+                _apiLink =
+                    'https://raw.githubusercontent.com/tonynguyen98/Fake-JSON-Server/master/test.json'; //level 1
+              } else if (userLevel == 2) {
+                _apiLink =
+                    'https://raw.githubusercontent.com/tonynguyen98/Fake-JSON-Server/master/test.json'; //level 2
+              } else {
+                _apiLink =
+                    'https://raw.githubusercontent.com/tonynguyen98/Fake-JSON-Server/master/excerciseList.json'; //default
+              }
             } else if (motivation == 'I want to lose weight') {
-              apiLink =
-                  'http://www.json-generator.com/api/json/get/cqeKauYdCG?indent=2'; //TODO: Change this link this a different one that contains a premade cardio workout
+              if (userLevel == 1) {
+                _apiLink =
+                    'https://raw.githubusercontent.com/tonynguyen98/Fake-JSON-Server/master/test.json'; //level 1
+              } else if (userLevel == 2) {
+                _apiLink =
+                    'https://raw.githubusercontent.com/tonynguyen98/Fake-JSON-Server/master/test.json'; //level 2
+              } else {
+                _apiLink =
+                    'https://raw.githubusercontent.com/tonynguyen98/Fake-JSON-Server/master/excerciseList.json';
+              } //default
             } else {
-              apiLink =
-                  'http://www.json-generator.com/api/json/get/bTNIKaWSqa?indent=2'; //default list
+              _apiLink =
+                  'https://raw.githubusercontent.com/tonynguyen98/Fake-JSON-Server/master/excerciseList.json'; //default list
             }
           },
         );
@@ -52,27 +72,57 @@ class _WorkoutPlanState extends State<WorkoutPlan> {
     );
   }
 
+  showAlertDialog(BuildContext context) {
+    Widget cancelButton = FlatButton(
+      child: Text('Cancel'),
+      onPressed: () {
+        Navigator.of(context, rootNavigator: true).pop();
+      },
+    );
+    Widget continueButton = FlatButton(
+      child: Text('Continue'),
+      onPressed: () {
+        currentLevelProgress = 0;
+        Navigator.of(context, rootNavigator: true).pop();
+        Navigator.pop(context);
+      },
+    );
+
+    AlertDialog alert = AlertDialog(
+      title: Text('WARNING'),
+      content: Text(
+          'You have not completed this workout, do you still want to exit?'),
+      actions: [
+        cancelButton,
+        continueButton,
+      ],
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
   Future<List<Exercises>> _getExercises() async {
-    var data = await http.get(apiLink);
+    var data = await http.get(_apiLink);
     var jsonData = json.decode(data.body);
 
     List<Exercises> exercises = [];
 
     for (var type in jsonData) {
       Exercises ex = Exercises(
-          type['id'],
-          type[
-              'exercise_name'], //TODO: when making your new JSON, please fix this to camelCase
-          type['body_part'], //TODO: ^
+          type['exerciseName'],
+          type['bodyPart'],
           type['strength'],
-          type['hypertrphy'],
-          type['cardio']);
-      String a = ex.bodyPart;
-      //if (a == ' chest') {
+          type['description'],
+          type['exerciseExample'],
+          type['muscleBody']);
       exercises.add(ex);
-      //} //TODO: Fix this to work with filters
     }
-    print(exercises.length);
+    _planLength = exercises.length;
     return exercises;
   }
 
@@ -82,37 +132,59 @@ class _WorkoutPlanState extends State<WorkoutPlan> {
       appBar: new AppBar(
         title: Text('Exercise List'),
       ),
-      body: Container(
-        child: FutureBuilder(
-          future: _getExercises(),
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.data == null) {
-              return Container(
-                child: Center(
-                  child: CircularProgressIndicator(),
-                ),
-              );
-            } else {
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text(snapshot.data[index].exerciseName),
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        new MaterialPageRoute(
-                          builder: (context) =>
-                              WorkoutDetailsPage(snapshot.data[index]),
-                        ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: FutureBuilder(
+              future: _getExercises(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.data == null) {
+                  return Container(
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                } else {
+                  return ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        title: Text(snapshot.data[index].exerciseName),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            new MaterialPageRoute(
+                              builder: (context) =>
+                                  WorkoutDetailsPage(snapshot.data[index]),
+                            ),
+                          );
+                          currentLevelProgress++;
+                        },
                       );
                     },
                   );
-                },
-              );
-            }
-          },
-        ),
+                }
+              },
+            ),
+          ),
+          MaterialButton(
+            minWidth: double.infinity,
+            height: 50.0,
+            onPressed: () {
+              if (currentLevelProgress <= _planLength) {
+                showAlertDialog(context);
+              } else {
+                Navigator.pop(context);
+                currentLevelProgress = 0;
+              }
+            },
+            child: Text(
+              'I\'m Done'.toUpperCase(),
+            ),
+            color: Colors.lightBlue,
+            textColor: Colors.white,
+          ),
+        ],
       ),
     );
   }
