48c3655fd0465dbdb4824057b4f614927362a9b2
Tony <tonynguyen0925@gmail.com>
2020-04-21 17:30:31 -0700

    fixed centering UI bug for reals this time

--- lib/pages/onboardingPages/get-started.dart
+++ lib/pages/onboardingPages/get-started.dart
@@ -9,72 +9,74 @@ class GetStarted extends StatelessWidget {
       backgroundColor: Colors.lightBlue[50],
       body: SafeArea(
         child: Scrollbar(
-          child: Container(
-            padding: EdgeInsets.fromLTRB(25, 100, 25, 25),
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  Image(
-                    image: AssetImage('assets/getStartedImage.png'),
-                    width: 380.0,
-                    height: 360.0,
-                    fit: BoxFit.fill,
-                  ),
-                  Text(
-                    'Welcome to Got Gainz',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      color: Colors.orange[800],
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.bold,
+          child: Center(
+            child: Container(
+              padding: EdgeInsets.fromLTRB(25, 100, 25, 25),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage('assets/getStartedImage.png'),
+                      width: 380.0,
+                      height: 360.0,
+                      fit: BoxFit.fill,
                     ),
-                  ),
-                  Text(
-                    'Your personal fitness trainer is waiting for you.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontStyle: FontStyle.italic,
-                      color: Colors.black87,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                  Text(
-                    'A few things before you start working out.',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16.0,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                  MaterialButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18.0),
-                      side: BorderSide(color: Colors.deepOrange),
-                    ),
-                    minWidth: 30.0,
-                    height: 50.0,
-                    color: Colors.orange[700],
-                    textColor: Colors.white,
-                    child: Text(
-                      'Get Started'.toUpperCase(),
+                    Text(
+                      'Welcome to Got Gainz',
+                      textAlign: TextAlign.left,
                       style: TextStyle(
+                        color: Colors.orange[800],
+                        fontSize: 30.0,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    onPressed: () {
-                      Navigator.of(context).pushNamed(
-                        '/second',
-                      );
-                    },
-                  ),
-                ],
+                    Text(
+                      'Your personal fitness trainer is waiting for you.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.black87,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Text(
+                      'A few things before you start working out.',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.0,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    MaterialButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0),
+                        side: BorderSide(color: Colors.deepOrange),
+                      ),
+                      minWidth: 30.0,
+                      height: 50.0,
+                      color: Colors.orange[700],
+                      textColor: Colors.white,
+                      child: Text(
+                        'Get Started'.toUpperCase(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pushNamed(
+                          '/second',
+                        );
+                      },
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
